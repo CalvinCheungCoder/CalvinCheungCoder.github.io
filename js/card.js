25876efb46ebbe82a370937aa961cf12ff6ab308
Calvin <984382258@qq.com>
2020-06-22 15:27:54 +0800

    Site updated: 2020-06-22 15:27:53

--- js/card.js
+++ js/card.js
@@ -1,30 +1,11 @@
-function setBookId(id,ratingStar,starNumber,divname){
-    $.ajax({
-        type: 'GET',
-        url: 'https://douban.uieee.com/v2/book/' + id,
-        dataType: 'JSON',
-        error: function () {
-            
-        },
-        success: function (res) {
-         htmlstr = '<p><div class="post-cardpreview"><div class="post-cardpreview--meta"><div class="post-cardpreview--middle"><h4 class="post-cardpreview--title"><a target="_blank" href="' + res.alt +'">'+ res.title +' (' + res.pubdate + ')</a></h4><div class="rating"><div class="rating-star allstar'+starNumber+'"></div><div class="rating-average">'+ ratingStar +'</div></div><time class="post-cardpreview--date">作者: '+ res.author +' </time><section style="max-height:75px;overflow:hidden;" class="post-cardpreview--excerpt">' + res.summary +'</section></div></div><img loading="lazy" class="post-cardpreview--image" src="' + res.image + '"></div></p>'
-         $('#' + divname).append(htmlstr)
-        }
-    });
+function setBookInfo(ratingStar,starNumber,divname,alt,image,title,author,summary){
+    // alt 跳转，title 标题，author 作者，summary 简介， image 封面
+    htmlstr = '<p><div class="post-cardpreview"><div class="post-cardpreview--meta"><div class="post-cardpreview--middle"><h4 class="post-cardpreview--title"><a target="_blank" href="' + alt +'">'+ title + '</a></h4><div class="rating"><div class="rating-star allstar'+starNumber+'"></div><div class="rating-average">'+ ratingStar +'</div></div><time class="post-cardpreview--date">作者: '+ author +' </time><section style="max-height:75px;overflow:hidden;" class="post-cardpreview--excerpt">' + summary +'</section></div></div><img loading="lazy" class="post-cardpreview--image" src="' + image + '"></div></p>'
+    document.getElementById(divname).innerHTML = htmlstr
 }
 
-function setMovieId(movieId,ratingStar,starNumber,divname){
-    $.ajax({
-        type: 'GET',
-        url: 'https://douban.uieee.com/v2/movie/subject/' + movieId,
-        dataType: 'JSON',
-        error: function () {
-            
-        },
-        success: function (res) {
-            htmlstr = '<p><div class="post-cardpreview"><div class="post-cardpreview--meta"><div class="post-cardpreview--middle"><h4 class="post-cardpreview--title"><a target="_blank" href="' + res.alt +'">'+ res.title + res.original_title +' (' + res.year + ')' + '</a></h4><div class="rating"><div class="rating-star allstar'+starNumber+'"></div><div class="rating-average">'+ ratingStar +'</div></div><time class="post-cardpreview--date">导演: '+ res.writers[0].name +' </time><section style="max-height:75px;overflow:hidden;" class="post-cardpreview--excerpt">' + res.summary +'</section></div></div><img loading="lazy" class="post-cardpreview--image" src="' + res.images['medium'] + '"></div></p>'
-            $('#' + divname).append(htmlstr)
-        }
-    });
+function setMovieInfo(ratingStar,starNumber,divname,alt,image,title,writers,summary){
+    // alt 跳转，title 标题，writers 导演，summary 简介， image 封面
+    htmlstr = '<p><div class="post-cardpreview"><div class="post-cardpreview--meta"><div class="post-cardpreview--middle"><h4 class="post-cardpreview--title"><a target="_blank" href="' + alt +'">'+ title + '</a></h4><div class="rating"><div class="rating-star allstar'+starNumber+'"></div><div class="rating-average">'+ ratingStar +'</div></div><time class="post-cardpreview--date">导演: '+ writers +' </time><section style="max-height:75px;overflow:hidden;" class="post-cardpreview--excerpt">' + summary +'</section></div></div><img loading="lazy" class="post-cardpreview--image" src="' + image + '"></div></p>'
+    document.getElementById(divname).innerHTML = htmlstr
 }
-
